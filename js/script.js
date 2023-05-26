3a2426dc63255a7412dbc033eaeaf77463ecee5d
marianaarodrigues <marianaggl@gmail.com>
2023-05-26 00:01:59 -0300

    cotinuação

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-function calcularIdade() {
+function calcularPercapita() {
+
+    // Cálculo da idade
     const hoje = new Date()
     const nasc = new Date(document.getElementById("datanasc").value)
     let anosCompletos = hoje.getFullYear() - nasc.getFullYear()
@@ -6,7 +8,19 @@ function calcularIdade() {
     if (mes < 0 || (mes === 0 && hoje.getDate() < nasc.getDate())) {
         anosCompletos--
     }
-    let resposta = "Sua idade é " + anosCompletos + " anos"
-    console.log(resposta)
-    document.getElementById("suaidade").innerText = resposta
+
+    // Cálculo da renda per capita
+
+    let rendatot = parseFloat(document.getElementById('renda').value)
+    let morador = parseFloat(document.getElementById('moradores').value)
+    if (morador == 0) {
+        resultadodiv = 'Dados inválidos'
+        document.getElementById('resdivisao').innerHTML = resultadodiv
+    }
+    else {
+        let resdiv = (parseFloat(a / b)).toFixed(2)
+        result = 'Resultado: ' + resdiv
+        document.getElementById('resdivisao').innerHTML = result
+    }
+
 }
