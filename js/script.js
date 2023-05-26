9a898fc3f6f4281ca3b809b940c8d51aa5394056
marianaarodrigues <marianaggl@gmail.com>
2023-05-26 15:24:42 -0300

    continuação

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
-function calcularPercapita() {
+function verificarPrematricula() {
 
-    // Cálculo da idade
+    //Calcular idade
+
     const hoje = new Date()
-    const nasc = new Date(document.getElementById("datanasc").value)
+    const nasc = new Date(document.getElementById('datanasc').value)
     let anosCompletos = hoje.getFullYear() - nasc.getFullYear()
     const mes = hoje.getMonth() - nasc.getMonth()
     if (mes < 0 || (mes === 0 && hoje.getDate() < nasc.getDate())) {
         anosCompletos--
     }
 
-    // Cálculo da renda per capita
+    //Calcular renda per capita
 
-    let rendatot = parseFloat(document.getElementById('renda').value)
-    let morador = parseFloat(document.getElementById('moradores').value)
-    if (morador == 0) {
-        resultadodiv = 'Dados inválidos'
-        document.getElementById('resdivisao').innerHTML = resultadodiv
+    let rendaTotal = parseFloat(document.getElementById('renda').value)
+    let quantMoradores = parseFloat(document.getElementById('moradores').value)
+    if (quantMoradores <= 0) {
+        mensagem = 'Dados inválidos'
+        document.getElementById('resposta').innerHTML = mensagem
+        return
+    }
+    let perCapita = (parseFloat(rendaTotal / quantMoradores)).toFixed(2)
+
+    //Verificar critérios
+
+    if (anosCompletos >= 16 && perCapita <= 1.5 * 1320) {
+        mensagem = 'Pré-matrícula aprovada, favor entrar em contato através do número (62) 3333-3333'
+        document.getElementById('resposta').innerHTML = mensagem
     }
     else {
-        let resdiv = (parseFloat(a / b)).toFixed(2)
-        result = 'Resultado: ' + resdiv
-        document.getElementById('resdivisao').innerHTML = result
+        mensagem = 'Você não atende aos critérios do Programa SENAC de Gratuidade'
+        document.getElementById('resposta').innerHTML = mensagem
     }
-
 }
