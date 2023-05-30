0a91341cb05dd29115a702ef247ecc4caf983674
marianaarodrigues <marianaggl@gmail.com>
2023-05-30 11:27:12 -0300

    continuação

--- js/script.js
+++ js/script.js
@@ -39,3 +39,13 @@ function verificarPrematricula() {
         document.getElementById('resposta').innerHTML = mensagem
     }
 }
+
+function calcularMedia() {
+
+    let n1 = parseFloat(document.getElementById('nota1').value)
+    let n2 = parseFloat(document.getElementById('nota2').value)
+    let n3 = parseFloat(document.getElementById('nota3').value)
+    let media = (n1 + n2 + n3) / 3
+
+    return media
+}
